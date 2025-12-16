87d87b5f9c67dd9b4a65626c6c2386e66fe7fda8
Linus David Martinez Garcia <linusmartinez00@gmail.com>
2025-12-16 13:49:51 -0600

    Removed test linters from workflow

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,29 +1,12 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+// ESLint desactivado - todas las reglas deshabilitadas
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['**/*']), // Ignorar todos los archivos
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      // Todas las reglas desactivadas
     },
   },
 ])
